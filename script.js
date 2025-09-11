1b1c93a75ce03fa7664c75fb596fa3d5fc18bb44
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-11 09:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const btn=document.getElementById("button")
+btn.addEventListener("click",selectionColor)
+function selectionColor(){
+    const select = document.getElementById("colorSelect");
+    const selectedOption = select.options[select.selectedIndex];
+    if (selectedOption) {
+        selectedOption.remove();
+    }
+}
